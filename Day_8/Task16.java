362b9be5729ed7778ccf591714241e11a675311d
Kavya Trivedi <kavyabt24@gmail.com>
2024-12-28 14:13:24 +0530

    Updated the code for task 16

--- Day_8/Task16.java
+++ Day_8/Task16.java
@@ -1,13 +1,37 @@
-// Create a function that accepts two arguments: the number of dice rolled, and the outcome of the roll. The function returns the number of possible combinations that could produce that outcome. The number of dice can vary from 1 to 6.
-// Ex.
-// User Input: 3 and 4. 
-// Output: 3
-// 1 + 1 + 2, 1 + 2 + 1, 2 + 1 + 1
-// User Input: 6 and 20. 
-// Output: 4221
+import java.util.Scanner;
 
 public class Task16 {
+
     public static void main(String[] args) {
-        // 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of dices rolled (1-6): ");
+        int dices = sc.nextInt();
+        System.out.print("Enter the outcome of roll: ");
+        int outcome = sc.nextInt();
+
+        System.out.println(findDiceCombinations(dices, outcome));
+    }
+
+    public static int findDiceCombinations(int numDice, int outcome) {
+        if (numDice < 1 || outcome < numDice || outcome > numDice * 6) {
+            return 0; // Impossible outcome
+        }
+
+        return countCombinations(numDice, outcome);
+    }
+
+    private static int countCombinations(int diceLeft, int target) {
+
+        // System.out.println("Method call: DiceLeft = " + diceLeft + ", Target = " + target);
+
+        if (diceLeft == 0) {
+            return target == 0 ? 1 : 0;
+        }
+
+        int count = 0;
+        for (int i = 1; i <= 6; i++) {
+            count += countCombinations(diceLeft - 1, target - i);
+        }
+        return count;
     }
 }
